f505a84801df74b01ee35c12b88681fc9348083d
Anton Ustyuzhanin <au@hellofresh.com>
2017-11-23 02:13:59 +0100

    Modify scripts

--- ci/scripts/build-images.sh
+++ ci/scripts/build-images.sh
@@ -6,10 +6,12 @@ CHANGES=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD
 [ -n "$(grep '^2.2.1.0' <<< "$CHANGES")" ] && BUILD_22=1
 [ -n "$(grep '^2.3.2.0' <<< "$CHANGES")" ] && BUILD_23=1
 
-if [[ -z "$BUILD_22" ]]; then
+env | sort
+
+if [[ -n "$BUILD_22" ]]; then
   docker build -t ${IMAGE}:2.2.1.0 2.2.1.0/
 fi
 
-if [[ -z "$BUILD_23" ]]; then
+if [[ -n "$BUILD_23" ]]; then
   docker build -t ${IMAGE}:2.3.2.0 2.3.2.0/
 fi
--- ci/scripts/push-images.sh
+++ ci/scripts/push-images.sh
@@ -6,11 +6,13 @@ CHANGES=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD
 [ -n "$(grep '^2.2.1.0' <<< "$CHANGES")" ] && BUILD_22=1
 [ -n "$(grep '^2.3.2.0' <<< "$CHANGES")" ] && BUILD_23=1
 
+env | sort
+
 docker login -u="${QUAY_USER}" -p="${QUAY_PASS}" quay.io
-if [ -z "$BUILD_22" ]; then
-  docker build -t ${IMAGE}:2.2.1.0 2.2.1.0/
+if [ -n "$BUILD_22" ]; then
+  docker push ${IMAGE}:2.2.1.0 2.2.1.0/
 fi
 
-if [ -z "$BUILD_23" ]; then
-  docker build -t ${IMAGE}:2.3.2.0 2.3.2.0/
+if [ -n "$BUILD_23" ]; then
+  docker push ${IMAGE}:2.3.2.0 2.3.2.0/
 fi
