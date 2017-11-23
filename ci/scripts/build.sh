1ac07a7be3e9e26677f75c4e95c51f1d6ca144f1
Anton Ustyuzhanin <au@hellofresh.com>
2017-11-23 11:15:36 +0100

    Merge script

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-ci/scripts/build-images.sh
+if [[ ! "${TRAVIS}" ]]; then
+  echo "This script should be only run from travis-ci. Exiting."
+  exit 0
+fi
 
-if [[ -z "${TRAVIS_PULL_REQUEST}" ]]; then
-  ci/scripts/push-images.sh
+CHANGES=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
+[ -n "$(grep "^${VERSION}" <<< "$CHANGES")" ] && BUILD_REQUIRED=1
+
+if [[ -z ${BUILD_REQUIRED} ]]; then
+  echo "Version ${VERSION} wasn't changed. Nothing to do."
+  exit 0
+fi
+
+echo docker build -t ${IMAGE}:${VERSION} ${VERSION}/
+
+if [[ $TRAVIS_PULL_REQUEST == "false" ]]; then
+  echo docker login -u="${QUAY_USER}" -p="${QUAY_PASS}" quay.io
+  echo docker push ${IMAGE}:${VERSION}
 else
   echo "Pull request build, don't push images"
 fi
