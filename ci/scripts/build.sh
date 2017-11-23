3895ae5192ca1d545f2907aa856c4aae38463776
Anton Ustyuzhanin <au@hellofresh.com>
2017-11-23 02:04:12 +0100

    Add debug info

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+set -eux
+
 ci/scripts/build-images.sh
 [[ $TRAVIS_PULL_REQUEST -ne "1" ]] && ci/scripts/push-images.sh
--- ci/scripts/push-images.sh
+++ ci/scripts/push-images.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eux
+
 CHANGES=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))  
 [ -n "$(grep '^2.2.1.0' <<< "$CHANGES")" ] && BUILD_22=1
 [ -n "$(grep '^2.3.2.0' <<< "$CHANGES")" ] && BUILD_23=1
