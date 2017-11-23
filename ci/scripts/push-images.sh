330acdda7e11bb390405cbe8a08bb4159ff9edf9
Anton Ustyuzhanin <au@hellofresh.com>
2017-11-23 02:24:19 +0100

    Look for VERSION

--- ci/scripts/push-images.sh
+++ ci/scripts/push-images.sh
@@ -3,16 +3,9 @@
 set -eux
 
 CHANGES=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD master))  
-[ -n "$(grep '^2.2.1.0' <<< "$CHANGES")" ] && BUILD_22=1
-[ -n "$(grep '^2.3.2.0' <<< "$CHANGES")" ] && BUILD_23=1
+[ -n "$(grep "^${VERSION}" <<< "$CHANGES")" ] && BUILD_REQUIRED=1
 
-env | sort
-
-docker login -u="${QUAY_USER}" -p="${QUAY_PASS}" quay.io
-if [ -n "$BUILD_22" ]; then
-  docker push ${IMAGE}:2.2.1.0 2.2.1.0/
-fi
-
-if [ -n "$BUILD_23" ]; then
-  docker push ${IMAGE}:2.3.2.0 2.3.2.0/
+if [ -n "${BUILD_REQUIRED}" ]; then
+  docker login -u="${QUAY_USER}" -p="${QUAY_PASS}" quay.io
+  docker push ${IMAGE}:${VERSION}
 fi
