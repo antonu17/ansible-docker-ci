ce7d0be8d80155e37ba4456a86ea8febaab50a01
Anton Ustyuzhanin <au@hellofresh.com>
2017-11-23 02:08:03 +0100

    Add bash test [[

--- ci/scripts/build-images.sh
+++ ci/scripts/build-images.sh
@@ -6,10 +6,10 @@ CHANGES=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD
 [ -n "$(grep '^2.2.1.0' <<< "$CHANGES")" ] && BUILD_22=1
 [ -n "$(grep '^2.3.2.0' <<< "$CHANGES")" ] && BUILD_23=1
 
-if [ -z "$BUILD_22" ]; then
+if [[ -z "$BUILD_22" ]]; then
   docker build -t ${IMAGE}:2.2.1.0 2.2.1.0/
 fi
 
-if [ -z "$BUILD_23" ]; then
+if [[ -z "$BUILD_23" ]]; then
   docker build -t ${IMAGE}:2.3.2.0 2.3.2.0/
 fi
